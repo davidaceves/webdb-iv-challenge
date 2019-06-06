93981dbd52b0311bb011c4a44cbbc1e36c4f91bb
David <digital0david@gmail.com>
2019-06-05 23:00:49 -0400

    get dishes

--- data/dishes/dishRouter.js
+++ data/dishes/dishRouter.js
@@ -5,4 +5,15 @@ const knex = require("knex");
 const knexConfig = require("../../knexfile.js");
 const db = knex(knexConfig.development);
 
+router.get("/", (req, res) => {
+  db("dishes")
+    .then(dishes => {
+      res.status(200).json(dishes);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
