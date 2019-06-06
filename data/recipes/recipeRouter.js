f5b61e1f70843508cb85ce5a4a48f5188e6d5b55
David <digital0david@gmail.com>
2019-06-06 00:12:05 -0400

    get recipes

--- data/recipes/recipeRouter.js
+++ data/recipes/recipeRouter.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const knex = require("knex");
+
+const knexConfig = require("../../knexfile.js");
+const db = knex(knexConfig.development);
+
+router.get("/", (req, res) => {
+  db.select("dishes.name AS dish", "recipes.name AS recipe")
+    .from("dishes", "recipes")
+    .innerJoin("recipes", "recipes.dish_id", "dishes.id")
+    .then(recipes => {
+      res.status(200).json(recipes);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
